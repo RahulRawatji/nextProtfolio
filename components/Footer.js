5e259a8dec1806c9d4ce640522d68f9432ebb660
rahulrawat <rahulrawatwork@gmail.com>
2022-12-09 12:01:31 +0530

    fix: :bug: linit fix

--- components/Footer.js
+++ components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer>
         <h4>Copyright 2022 @developedByRahul</h4>
         <ul>
-          <li><a href="https://drive.google.com/file/d/1JlG1H9NbpTnslBjUPBww4guZOPOzv_ZM/view?usp=sharing" target="_blank"><ImProfile size={25} title="Resume"/></a></li>
-          <li><a href="https://github.com/RahulRawatji"><SlSocialGithub size={25}/></a></li>
-          <li><a href="https://www.linkedin.com/in/rahul-rawat21/"><SlSocialLinkedin size={25}/></a></li>
+          <li><a href="https://drive.google.com/file/d/1JlG1H9NbpTnslBjUPBww4guZOPOzv_ZM/view?usp=sharing" target="_blank" rel="noreferrer"><ImProfile size={25} title="Resume"/></a></li>
+          <li><a href="https://github.com/RahulRawatji" rel="noreferrer"><SlSocialGithub size={25}/></a></li>
+          <li><a href="https://www.linkedin.com/in/rahul-rawat21/" rel="noreferrer"><SlSocialLinkedin size={25}/></a></li>
         </ul>
     </footer>
   )
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ export default function Navbar () {
     return <nav>
         <h1><a href="#">rahulrawat</a></h1>
         <ul>
-            <li><a href="https://drive.google.com/file/d/1JlG1H9NbpTnslBjUPBww4guZOPOzv_ZM/view?usp=sharing" target="_blank"><ImProfile size={25} title="Resume"/></a></li>
-            <li><a href="https://github.com/RahulRawatji" target="_blank"><SlSocialGithub size={25} title="Github"/></a></li>
-            <li><a href="https://www.linkedin.com/in/rahul-rawat21/" target="_blank"><SlSocialLinkedin size={25} title="LinkedIn"/></a></li>
+            <li><a href="https://drive.google.com/file/d/1JlG1H9NbpTnslBjUPBww4guZOPOzv_ZM/view?usp=sharing" target="_blank" rel="noreferrer"><ImProfile size={25} title="Resume"/></a></li>
+            <li><a href="https://github.com/RahulRawatji" target="_blank" rel="noreferrer"><SlSocialGithub size={25} title="Github"/></a></li>
+            <li><a href="https://www.linkedin.com/in/rahul-rawat21/" target="_blank" rel="noreferrer"><SlSocialLinkedin size={25} title="LinkedIn"/></a></li>
         </ul>
     </nav>
 }
