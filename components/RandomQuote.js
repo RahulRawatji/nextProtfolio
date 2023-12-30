17620dfb29f6a8e3d1a2885d471b6314dee1ff65
rahulrawat <rahul@concerto.ai>
2023-12-30 10:44:56 +0530

    fix: fix for quotes

--- components/RandomQuote.js
+++ components/RandomQuote.js
@@ -1,33 +1,32 @@
-import React, { useEffect, useState, useRef } from 'react'
-import axios from 'axios';
-import { AiOutlineReload } from 'react-icons/ai';
-import { useInView ,motion } from 'framer-motion';
+import React, { useEffect, useState, useRef } from "react"
+import axios from "axios"
+import { AiOutlineReload } from "react-icons/ai"
+import { useInView, motion } from "framer-motion"
 
 const RandomQuote = () => {
-  const [quoteData, set] = useState({});
- 
-  useEffect(() => {
-    fetchRandomQuote();
-  }, []);
+  const [quoteData, set] = useState({})
 
+  useEffect(() => {
+    fetchRandomQuote()
+  }, [])
 
   const fetchRandomQuote = async () => {
-
-    const response = await axios("https://type.fit/api/quotes");
-    const data = response.data;
-    const randomNumber = Math.floor(Math.random() * 1643);
-    set(data[randomNumber]);
-  };
+    const response = await axios("https://type.fit/api/quotes")
+    const data = response.data
+    const randomNumber = Math.floor(Math.random() * 10)
+    set(data[randomNumber])
+  }
 
   return (
-    <motion.div initial={{opacity:0}} transition={{ delay: .5 }} whileInView={{ opacity:1, scale:.8}} viewport={{ once: true }}>
-    <h3 className='quote'>
-      {quoteData.text}
-    </h3>
-    <p className=''>
-      {quoteData.author}
-    </p>
-  </motion.div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      transition={{ delay: 0.5 }}
+      whileInView={{ opacity: 1, scale: 0.8 }}
+      viewport={{ once: true }}
+    >
+      <h3 className="quote">{quoteData?.text}</h3>
+      <p className="">{quoteData?.author}</p>
+    </motion.div>
   )
 }
 
